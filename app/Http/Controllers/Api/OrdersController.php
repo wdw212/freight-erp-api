1b7fcbaea3fe2c54b64eaf9b0308fa07543fcca3
zcy <3041105031@qq.com>
2025-04-06 23:41:09 +0800

    提交代码

--- app/Http/Controllers/Api/OrdersController.php
+++ app/Http/Controllers/Api/OrdersController.php
@@ -13,6 +13,7 @@ use App\Models\Order;
 use App\Models\OrderPayment;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class OrdersController extends Controller
 {
@@ -64,13 +65,43 @@ class OrdersController extends Controller
         return new OrderInfoResource($order->load(['orderPayments']));
     }
 
-    public function update()
+    /**
+     * 编辑
+     * @param OrderRequest $request
+     * @param Order $order
+     * @return OrderInfoResource
+     */
+    public function update(OrderRequest $request, Order $order): OrderInfoResource
     {
+        $data = $request->all();
+        $order->fill($request->all());
+        $order->update();
 
+        // 处理应付款
+        if (!empty($data['order_payments'])) {
+            $orderPayments = json_decode($data['order_payments'], true);
+            $orderPaymentRelations = [];
+            foreach ($orderPayments as $orderPayment) {
+                if (isset($orderPayment['id'])) {
+                    OrderPayment::query()->where('id', $orderPayment['id'])->update($orderPayment);
+                } else {
+                    $orderPaymentRelations[] = new OrderPayment($orderPayment);
+                }
+            }
+            $order->orderPayments()->saveMany($orderPaymentRelations);
+        }
+
+        return new OrderInfoResource($order);
     }
 
-    public function destroy(Order $order)
+    /**
+     * 删除
+     * @param Order $order
+     * @return Response
+     */
+    public function destroy(Order $order): Response
     {
-
+        $order->delete();
+        return response()->noContent();
     }
 }
