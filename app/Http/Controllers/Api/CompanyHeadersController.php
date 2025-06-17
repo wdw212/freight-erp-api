cd665b94e6536909d6b2caf4360eeff8adb16f9e
zcy <3041105031@qq.com>
2025-06-17 11:24:59 +0800

    优化公司抬头列表

--- app/Http/Controllers/Api/CompanyHeadersController.php
+++ app/Http/Controllers/Api/CompanyHeadersController.php
@@ -43,14 +43,14 @@ class CompanyHeadersController extends Controller
             $builder = $builder->where('document_user_id', $documentUserId);
         }
 
-        if (!empty($companyType)) {
-            $builder = $builder->orWhere(function ($query) use ($companyType) {
-                $companyType = json_decode($companyType, true);
-                foreach ($companyType as $type) {
-                    $query->orWhereJsonContains('company_type', $type);
-                }
-            });
-        }
+//        if (!empty($companyType)) {
+//            $builder = $builder->orWhere(function ($query) use ($companyType) {
+//                $companyType = json_decode($companyType, true);
+//                foreach ($companyType as $type) {
+//                    $query->orWhereJsonContains('company_type', $type);
+//                }
+//            });
+//        }
 
         if ((int)$isPaginate === 1) {
             $companyHeaders = $builder->paginate($isPaginate);
