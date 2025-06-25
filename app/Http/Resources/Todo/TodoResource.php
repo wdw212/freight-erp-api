c248c7bb8eabd9d4f9baf73a057f5130f1f158f2
zcy <3041105031@qq.com>
2025-06-25 23:01:45 +0800

    提交代码

--- app/Http/Resources/Todo/TodoResource.php
+++ app/Http/Resources/Todo/TodoResource.php
@@ -5,6 +5,11 @@ namespace App\Http\Resources\Todo;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property mixed $id
+ * @property mixed $title
+ * @property mixed $status
+ */
 class TodoResource extends JsonResource
 {
     /**
@@ -14,6 +19,10 @@ class TodoResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'status' => $this->status,
+        ];
     }
 }
