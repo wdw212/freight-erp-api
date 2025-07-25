959b53cf40f7dbb13e95e2de161f7dad0ae82dc0
zcy <3041105031@qq.com>
2025-07-25 18:18:57 +0800

    提交代码

--- app/Http/Controllers/Api/CompanyHeadersController.php
+++ app/Http/Controllers/Api/CompanyHeadersController.php
@@ -58,7 +58,7 @@ class CompanyHeadersController extends Controller
         }
 
         if (!empty($companyTypeId)) {
-            $builder = $builder->orWhereJsonContains('company_type', $companyTypeId);
+            $builder = $builder->orWhereJsonContains('company_type', (int)$companyTypeId);
         }
         if ((int)$isPaginate === 1) {
             $companyHeaders = $builder->paginate();
