74da2c9e34f70b26568b1aa42eb9cc841ba628e2
zcy <3041105031@qq.com>
2025-07-26 23:51:05 +0800

    提交代码

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,13 @@ Route::get('/test2', static function () {
     $data['company_name'] = '宁波凌亚国际物流有限公司宁波分公司111';
     $builder = CompanyHeader::query()
         ->where('company_name', $data['company_name']);
-
     $businessUserIds = $builder->clone()->pluck('business_user_ids')->toArray();
-    dd(array_unique(Arr::collapse($businessUserIds)));
+    $businessUserIds = array_unique(Arr::collapse($businessUserIds));
+    $test = [1, 10, 11];
 
+    foreach ($test as $item) {
+        if (in_array($item, $businessUserIds)) {
+            throw new \App\Exceptions\InvalidRequestException('重复数据，请重试！');
+        }
+    }
 });
