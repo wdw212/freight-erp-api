212597dde023977215727cf419219acb7bd3d79f
bohelan <3041105031@qq.com>
2025-08-13 14:49:49 +0800

    费用类型

--- app/Http/Requests/FeeTypeRequest.php
+++ app/Http/Requests/FeeTypeRequest.php
@@ -2,27 +2,22 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class FeeTypeRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required',
+            'type' => 'required',
+            'sort' => 'required',
         ];
     }
 }
