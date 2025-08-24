47f422eccef2f8d4a45aefc4b572ff7badd325b3
bohelan <3041105031@qq.com>
2025-08-24 15:30:31 +0800

    增加单据认领

--- app/Http/Requests/OrderRequest.php
+++ app/Http/Requests/OrderRequest.php
@@ -15,10 +15,10 @@ class OrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'payment_method' => 'required',
-            'cutoff_status' => 'required',
-            'bl_status' => 'required',
-            'seller_id' => 'required',
+//            'payment_method' => 'required;',
+//            'cutoff_status' => 'required',
+//            'bl_status' => 'required',
+//            'seller_id' => 'required',
         ];
     }
 
@@ -28,10 +28,10 @@ class OrderRequest extends FormRequest
     public function attributes(): array
     {
         return [
-            'payment_method' => '支付方式',
-            'cutoff_status' => '截单状态',
-            'bl_status' => '提单状态',
-            'seller_id' => '销货单位'
+//            'payment_method' => '支付方式',
+//            'cutoff_status' => '截单状态',
+//            'bl_status' => '提单状态',
+//            'seller_id' => '销货单位'
         ];
     }
 }
