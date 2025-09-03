064cfa683b95db23d8c44d9b80aaff98fe27bb52
bohelan <3041105031@qq.com>
2025-09-03 12:41:00 +0800

    订单增加字段

--- database/migrations/2025_09_03_122901_add_is_finish_to_orders_table.php
+++ database/migrations/2025_09_03_122901_add_is_finish_to_orders_table.php
@@ -13,7 +13,7 @@ return new class extends Migration {
         Schema::table('orders', static function (Blueprint $table) {
             $table->tinyInteger('is_finish')->comment('是否结单 0否 1是')->default(0);
             $table->unsignedBigInteger('entered_port_wharf_id')->comment('进港码头')->nullable();
-            $table->tinyInteger('insurance_id')->comment('保险 0不需要 1需要 2已做')->default(0);
+            $table->tinyInteger('insurance')->comment('保险 0不需要 1需要 2已做')->default(0);
             $table->tinyInteger('is_allowed')->comment('是否放行 0未放行 1已放行')->default(0);
         });
     }
@@ -27,7 +27,7 @@ return new class extends Migration {
             $table->dropColumn([
                 'is_finish',
                 'entered_port_wharf_id',
-                'insurance_id',
+                'insurance',
                 'is_allowed',
             ]);
         });
