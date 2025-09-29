1129f75fcefd0ef964d87cf24cde664da49d2fa9
bohelan <3041105031@qq.com>
2025-09-29 13:36:49 +0800

    提交测试

--- app/Observers/OrderBlInfoObserver.php
+++ app/Observers/OrderBlInfoObserver.php
@@ -17,9 +17,34 @@ class OrderBlInfoObserver
     /**
      * Handle the OrderBlInfo "saved" event.
      */
-    public function saved(OrderBlInfo $orderBlInfo): void
+    public function saving(OrderBlInfo $orderBlInfo): void
     {
+        if ($orderBlInfo->sender_id === '') {
+            $orderBlInfo->sender_id = null; // 或 auth()->id()
+        } elseif (!is_null($orderBlInfo->sender_id) && !is_numeric($orderBlInfo->sender_id)) {
+            $orderBlInfo->sender_id= null;
+        } elseif (is_numeric($orderBlInfo->sender_id)) {
+            // 确保是整数类型
+            $orderBlInfo->sender_id = (int)$orderBlInfo->sender_id;
+        }
 
+        if ($orderBlInfo->receiver_id === '') {
+            $orderBlInfo->receiver_id = null; // 或 auth()->id()
+        } elseif (!is_null($orderBlInfo->receiver_id) && !is_numeric($orderBlInfo->receiver_id)) {
+            $orderBlInfo->receiver_id= null;
+        } elseif (is_numeric($orderBlInfo->receiver_id)) {
+            // 确保是整数类型
+            $orderBlInfo->receiver_id = (int)$orderBlInfo->receiver_id;
+        }
+
+        if ($orderBlInfo->notifier_id === '') {
+            $orderBlInfo->notifier_id = null;
+        } elseif (!is_null($orderBlInfo->notifier_id) && !is_numeric($orderBlInfo->notifier_id)) {
+            $orderBlInfo->notifier_id= null;
+        } elseif (is_numeric($orderBlInfo->notifier_id)) {
+            // 确保是整数类型
+            $orderBlInfo->notifier_id = (int)$orderBlInfo->notifier_id;
+        }
     }
 
     /**
