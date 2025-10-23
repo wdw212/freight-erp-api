75b273bcc80360ce6c62b17561ccdeb9e5d77a1d
bohelan <3041105031@qq.com>
2025-10-23 22:12:34 +0800

    提交测试

--- app/Http/Controllers/Api/WechatController.php
+++ app/Http/Controllers/Api/WechatController.php
@@ -8,6 +8,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\OfficialAccountService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WechatController extends Controller
 {
@@ -20,12 +21,26 @@ class WechatController extends Controller
         $app = (new OfficialAccountService())->getApp();
         $server = $app->getServer();
 
-        $server->with(function ($message, \Closure $next) {
-            return '谢谢关注！';
-
-            // 你的自定义逻辑
-            // return $next($message);
+        $server->addEventListener('subscribe', function ($message, \Closure $next) {
+            return '感谢您关注!';
         });
+
+        $server
+            ->with(function ($message, \Closure $next) {
+                // 你的自定义逻辑1
+                Log::info('打印message');
+                Log::info(json_encode($message, JSON_UNESCAPED_UNICODE));
+                return $next($message);
+            })
+            ->with(function ($message, \Closure $next) {
+                // 你的自定义逻辑2
+                return $next($message);
+            })
+            ->with(function ($message, \Closure $next) {
+                // 你的自定义逻辑3
+                return $next($message);
+            });
+
 
         return $server->serve();
     }
