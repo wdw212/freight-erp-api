4db87d4ab05111fc8aa90c228a73f2747231662c
bohelan <3041105031@qq.com>
2025-11-04 17:49:41 +0800

    提交测试

--- app/Http/Controllers/Api/InvoicesController.php
+++ app/Http/Controllers/Api/InvoicesController.php
@@ -47,7 +47,10 @@ class InvoicesController extends Controller
         $saleUscCode = $request->input('sale_usc_code');
         $cnyInvoiceItems = $request->input('cny_invoice_items');
         $usdInvoiceItems = $request->input('usd_invoice_items');
-
+        $cnyInvoiceItems = json_decode($cnyInvoiceItems, true);
+        $usdInvoiceItems = json_decode($usdInvoiceItems, true);
+        $invoiceItems = array_merge($cnyInvoiceItems, $usdInvoiceItems);
+        dd($invoiceItems);
         // 如果单据完成 修改订单信息
         if ((int)$isFinish === 1) {
             Order::query()->where('id', $orderId)->update([
@@ -72,10 +75,8 @@ class InvoicesController extends Controller
         $invoice->sale_entity_id = $saleEntityId;
         $invoice->sale_usc_code = $saleUscCode;
         $invoice->save();
-        
-        $cnyInvoiceItems = json_decode($cnyInvoiceItems, true);
-        $usdInvoiceItems = json_decode($usdInvoiceItems, true);
-        $invoiceItems = array_merge($cnyInvoiceItems, $usdInvoiceItems);
+
+
         $invoiceItemRelation = [];
         foreach ($invoiceItems as $item) {
             $invoiceItemRelation = new InvoiceItem($item);
