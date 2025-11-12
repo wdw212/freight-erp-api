55fe0481187df0bbec5b83d8e2a86f51db248aa6
bohelan <3041105031@qq.com>
2025-11-12 16:56:33 +0800

    提交代码

--- app/Http/Requests/InvoiceRequest.php
+++ app/Http/Requests/InvoiceRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class InvoiceRequest extends FormRequest
@@ -9,12 +10,22 @@ class InvoiceRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'invoice_type_id' => 'required'
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function messages(): array
+    {
+        return [
+            'invoice_type_id.required' => '发票类型不能为空!',
         ];
     }
 }
